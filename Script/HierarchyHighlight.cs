26b679bb34bed30eec3733941d1a3c297627f053
kalstjd96 <47016363+kalstjd96@users.noreply.github.com>
2023-02-11 20:55:53 +0900

    Script Upload

---
+++ Script/HierarchyHighlight.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HierarchyHighlight : MonoBehaviour
+{
+    public static HierarchyHighlight instance { get; private set; }
+    Material highlightMaterial;
+
+    List<MeshRenderer[]> targets;
+    Color[] defaultColors;
+    List<Material[]> defaultMaterials;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        targets = new List<MeshRenderer[]>();
+        defaultMaterials = new List<Material[]>();
+    }
+
+    void OnDestroy()
+    {
+        if (instance != null)
+            instance = null;
+    }
+
+    public void On(GameObject[] models)
+    {
+        targets.Insert(0, new MeshRenderer[models.Length]);
+        defaultMaterials.Insert(0, new Material[models.Length]);
+
+        for (int i = 0; i < models.Length; i++)
+        {
+            targets[0][i] = models[i].GetComponent<MeshRenderer>();
+            
+            defaultMaterials[0][i] = targets[0][i].material;
+        }
+        
+        for (int i = 0; i < targets[0].Length; i++)
+            targets[0][i].material = highlightMaterial;
+    }
+
+    public void Off()
+    {
+        for (int i = 0; i < targets.Count; i++)
+            for (int j = 0; j < targets[i].Length; j++)
+                targets[i][j].material = defaultMaterials[i][j];
+
+        targets.Clear();
+        defaultMaterials.Clear();
+    }
+
+    public void SetMaterial(Material material)
+    {
+        highlightMaterial = material;
+    }
+}
---
+++ Script/HierarchyTag.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HierarchyTag : MonoBehaviour
+{
+    public static HierarchyTag instance { get; private set; }
+
+    [SerializeField] Transform rightTagPrefab;
+    [SerializeField] Transform leftTagPrefab;
+
+    List<Transform> rightTagList;
+    List<Transform> leftTagList;
+    Dictionary<Transform, string> useTagDictionary;
+    Transform originalModel;
+    Vector3 center;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        rightTagList = new List<Transform>();
+        leftTagList = new List<Transform>();
+        useTagDictionary = new Dictionary<Transform, string>();
+    }
+
+    void OnDestroy()
+    {
+        if (instance != null)
+            instance = null;
+    }
+
+    void Start()
+    {
+        originalModel = SequenceManager.instance.currentEquip.transform;
+
+        MeshRenderer[] meshRenderers = originalModel.GetComponentsInChildren<MeshRenderer>();
+        Bounds bounds = meshRenderers[0].bounds;
+        for (int i = 1; i < meshRenderers.Length; i++)
+            bounds.Encapsulate(meshRenderers[i].bounds);
+
+        center = bounds.center;
+    }
+
+    public void On(GameObject models)
+    {
+        MeshRenderer[] meshRenderers = models.GetComponentsInChildren<MeshRenderer>();
+        Bounds bounds = meshRenderers[0].bounds;
+        for (int i = 1; i < meshRenderers.Length; i++)
+            bounds.Encapsulate(meshRenderers[i].bounds);
+
+        Transform currentTag = null;
+        float targetX = originalModel.InverseTransformPoint(bounds.center).x;
+        float centerX = originalModel.InverseTransformPoint(center).x;
+
+        if (targetX > centerX)
+        {
+            if (rightTagList.Count > 0)
+            {
+                currentTag = rightTagList[0];
+                useTagDictionary.Add(currentTag, "Right");
+                rightTagList.RemoveAt(0);
+            }
+            else
+            {
+                currentTag = Instantiate(rightTagPrefab);
+                useTagDictionary.Add(currentTag, "Right");
+            }
+        }
+        else
+        {
+            if (leftTagList.Count > 0)
+            {
+                currentTag = leftTagList[0];
+                useTagDictionary.Add(currentTag, "Left");
+                leftTagList.RemoveAt(0);
+            }
+            else
+            {
+                currentTag = Instantiate(leftTagPrefab);
+                useTagDictionary.Add(currentTag, "Left");
+            }
+        }
+
+        currentTag.gameObject.SetActive(true);
+        currentTag.position = bounds.center;
+        currentTag.GetComponentInChildren<Text>().text = models.name;
+    }
+
+    public void Off()
+    {
+        foreach (var key in useTagDictionary.Keys)
+        {
+            if (useTagDictionary[key].Equals("Right"))
+                rightTagList.Add(key);
+            else leftTagList.Add(key);
+            
+            key.gameObject.SetActive(false);
+        }
+
+        useTagDictionary.Clear();
+    }
+}
